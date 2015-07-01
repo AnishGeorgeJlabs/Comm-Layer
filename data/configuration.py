6d2aea23d7aa034a4bc29e13979c60deced8ab11
AnishGeorgeJlabs <anish.george@jlabs.co>
2015-07-01 18:34:26 +0530

    Refactoring done

--- data/configuration.py
+++ data/configuration.py
@@ -1,4 +1,7 @@
 config = {
     "app_queue": "AppTest",
-    "sms_queue": "SmsTest"
+    "sms_queue": "SmsTest",
+    "data_url": "https://docs.google.com/spreadsheets/d/1xjPWC3pBZVTDo4bYJAHkr7TyHyDO9YBS7XDbMCgmbIE/export?format=csv",
+    "content_url": "https://docs.google.com/spreadsheets/d/144fuYSOgi8md4n2Ezoj9yNMi6AigoXrkHA9rWIF0EDw/export?format=zip",
+    "sms_post_url": "http://www.smscountry.com/smscwebservice_bulk.aspx"
 }
