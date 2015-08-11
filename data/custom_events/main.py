b75627b4473b4475cd0bdde5a81543257aee72c9
bassoGeorge <anishgeorgehb@gmail.com>
2015-08-11 21:27:16 +0530

    fixes

--- data/custom_events/main.py
+++ data/custom_events/main.py
@@ -38,4 +38,4 @@ def execute_pipeline(pipeline, options):
             for k in cid_set:
                 extra_data[k] = res[k] + temp[k]        # Both are arrays
 
-    return [ data for k, data in enumerate(extra_data) ]
+    return [ data for k, data in extra_data.items() ]
