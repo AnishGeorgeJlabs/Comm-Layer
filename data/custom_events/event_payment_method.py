d764e078473f94dc9a209d8300f267fa78d805a2
AnishGeorgeJlabs <anish.george@jlabs.co>
2015-08-13 16:04:33 +0530

    Started working on payment_method

---
+++ data/custom_events/event_payment_method.py
@@ -0,0 +1,20 @@
+"""
+Event algorithms to get payment method from db
+"""
+import aux
+from . import connect_db
+
+def operate(options):
+    """ Driver for get_payment
+    Implements the query_event_driver specification
+    Note: payment_method has to be a single string value
+    """
+    mode = aux.get_mode(options)
+    if 'payment_method' in options:
+        return get_payment(mode, options['payment_method'])
+    else:
+        return None, None, None
+
+
+def get_payment(mode, payment_method):
+    pass
