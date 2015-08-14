75c67c62f04cc3189cb5b5e8ff97e66041f2dea6
AnishGeorgeJlabs <anish.george@jlabs.co>
2015-08-14 15:58:56 +0530

    bug fix on payment

--- data/custom_events/event_payment_method.py
+++ data/custom_events/event_payment_method.py
@@ -1,5 +1,7 @@
 """
 Event algorithms to get payment method from db
+
+Tested on Fri, 14 Aug, 03:58 AM
 """
 import aux
 from . import connect_db
@@ -19,7 +21,7 @@ def get_payment(mode, payment_method):
     query = """
     SELECT distinct(fk_customer), payment_method
     FROM sales_order
-    WHERE payment_method = %s""" % payment_method
+    WHERE payment_method = '%s'""" % payment_method
 
     return aux.typical_event_routing(mode, query, ['Payment Method'])
 
