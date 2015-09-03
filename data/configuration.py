4b7b5ea43c817ccd82713ce4526db98b39d09345
AnishGeorgeJlabs <anish.george@jlabs.co>
2015-09-03 12:54:57 +0530

    debugging fest

--- data/configuration.py
+++ data/configuration.py
@@ -7,8 +7,8 @@ config = {
     "data_url": "https://docs.google.com/spreadsheets/d/1xjPWC3pBZVTDo4bYJAHkr7TyHyDO9YBS7XDbMCgmbIE/export?format=csv",
     "content_url": "https://docs.google.com/spreadsheets/d/144fuYSOgi8md4n2Ezoj9yNMi6AigoXrkHA9rWIF0EDw/export?format=zip",
     "sms_post_url": "http://www.smscountry.com/smscwebservice_bulk.aspx",
-    # "logFile": "/home/asharma/Comm-Layer/logs/critical.log",
-    "logFile": "./logs/critical.log",
+    "logFile": "/home/asharma/Comm-Layer/logs/critical.log",
+    # "logFile": "./logs/critical.log",
     "logFormat": "[%(name)s: %(levelname)s] %(asctime)s -- %(message)s"
 }
 
