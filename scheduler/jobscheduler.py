7235c19f896ec620830d0158edcf5028203e8f3d
AnishGeorgeJlabs <anish.george@jlabs.co>
2015-09-11 15:06:08 +0530

    minor changes

--- scheduler/jobscheduler.py
+++ scheduler/jobscheduler.py
@@ -153,8 +153,8 @@ def _data_map(conf):
                     "lower_limit": int(dt[2]),
                     "upper_limit": int(dt[3])
                 }
-                if dt[1].startswith('external_database'):
-                    res['db_file'] = dt[1].replace('external_database', '')
+                if dt[1].startswith('external_database_'):
+                    res['db_file'] = dt[1].replace('external_database_', '')
                 else:
                     res['ref_id'] = int(dt[1])
             except Exception, e:
