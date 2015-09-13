2e7cbd4591b8113ec5208dd1616dd3032738e47b
bassoGeorge <anishgeorgehb@gmail.com>
2015-09-13 10:15:00 +0530

    minor fix

--- data/job_update_api.py
+++ data/job_update_api.py
@@ -2,10 +2,13 @@ import requests
 import json
 
 def _send(data):
-    api_url = "http://45.55.72.208/wadi/job_update"
-    headers = {'Content-type': 'application/json'}
-    r = requests.post(api_url, data=json.dumps(data), headers=headers)
-    return r.status_code == 200 and r.json().get('success', False)
+    try:
+        api_url = "http://45.55.72.208/wadi/job_update"
+        headers = {'Content-type': 'application/json'}
+        r = requests.post(api_url, data=json.dumps(data), headers=headers)
+        return r.status_code == 200 and r.json().get('success', False)
+    except:
+        return False
 
 def update_job_status(oid, **kwargs):
     if id is None or id == '':
